deae82c3607a38c926d6ab4046399b838e5d166a
tamurakeito <kit_tamtam@icloud.com>
2023-02-09 16:37:58 +0900

    コマンドの整理

---
+++ .init.sh
@@ -0,0 +1,8 @@
+echo '\x1b[36mプロジェクト名を入力しEnterキーを押してください\x1b[0m'
+read -p '> ' title
+yes | rm -r ./.git
+echo '...gitをリセットしました...'
+# packageにある名前を一括で変更する記述
+sed -i '' s/react-ts-template/${title}/ package*
+mv ../react-ts-template ../${title}
+echo '...プロジェクト名を設定しました...'
