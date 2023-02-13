e820c0b94380bf454652776174b70a4ba9b9401a
たむらけいと <63249584+tamurakeito@users.noreply.github.com>
2023-02-13 17:05:35 +0900

    Update .init.sh

--- .init.sh
+++ .init.sh
@@ -1,8 +1,13 @@
 echo '\x1b[36mプロジェクト名を入力しEnterキーを押してください\x1b[0m'
 read -p '> ' title
+
 rm -r ./.init.sh
+echo '# ${title}' > ./README.md
+echo '...init操作関連ファイルをクリアしました...'
+
 yes | rm -r ./.git
 echo '...gitをリセットしました...'
+
 # packageにある名前を一括で変更する記述
 sed -i '' s/react-ts-template/${title}/ package*
 mv ../react-ts-template ../${title}
