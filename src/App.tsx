3d2656c474a36d00409e463406d0c034baf64d32
tamurakeito <kit_tamtam@icloud.com>
2023-02-22 13:24:22 +0900

    yarn commit: リセットCSSの追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { RouteSelector } from "routes";
+import "the-new-css-reset/css/reset.css";
 
 function App() {
   return (
